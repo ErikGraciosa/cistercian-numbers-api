ad0ffb35253797e158a7db4429a64e5e9b45a2cf
ErikGraciosa <erikgraciosa@gmail.com>
2021-07-11 18:56:48 -0700

    testing format

--- utils/cistercianDecoder.js
+++ utils/cistercianDecoder.js
@@ -1,10 +1,22 @@
 export const cistercianDecoder = (number) => {
   console.log(number);
   if(number > 9999 || number < 1){
-    return 'Sorry number out of range, select a number betwee 1-9999';
+    return 'Sorry number out of range, select a number between 1-9999';
   }
 
-  let cistercianNumber = 'the number';
+  const ones = {
+    1: '   _ \n  |  \n  |  \n  |  \n     ',
+    2: '  |  \n  |  \n  |  ',
+    3: '  |  \n  |  \n  |  ',
+    4: '  |  \n  |  \n  |  ',
+    5: '  |  \n  |  \n  |  ',
+    6: '  |  \n  |  \n  |  ',
+    7: '  |  \n  |  \n  |  ',
+    8: '  |  \n  |  \n  |  ',
+    9: '  |  \n  |  \n  |  ',
+  };
+
+  let cistercianNumber = ones[1];
 
 
   return cistercianNumber;
